0d70e69014cbf4731f59293f8e5c06c02a241d01
Art <artbogdanovics@gmail.com>
2022-07-04 20:35:23 +0300

    added computerPlay

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
+
+let myArray = ['rock', 'paper', 'scissors'];
+
 function computerPlay () {
 
-    
+    let computerAnswer = myArray [Math.floor(Math.random() * myArray.length)];
+
+    console.log (computerAnswer);
+
 }
+
+computerPlay();
