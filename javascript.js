d13ed927d111800c48a6ec8abe920c6c4aa51d3b
Art <artbogdanovics@gmail.com>
2022-07-04 22:31:40 +0300

    added playRound

--- javascript.js
+++ javascript.js
@@ -1,22 +1,49 @@
 
-let myArray = ['rock', 'paper', 'scissors'];
+
 
 function computerPlay () {
-
+    let myArray = ['rock', 'paper', 'scissors'];
     let computerSelection = myArray [Math.floor(Math.random() * myArray.length)];
 
-    console.log (computerSelection);
+    return computerSelection;
 
 }
 
-/* computerPlay(); */
+
+computerPlay();
 
 
 let playerSelection = prompt ('Rock, Paper or Scissors?')
 
+function playRound (playerSelection, computerSelection) {
 
+    if (playerSelection === computerSelection) {
+        /* return "Tie!"; */
+        alert ("Tie!");
+     }
 
-function playRound () {
+     else if (playerSelection === 'paper' && computerSelection === 'rock') {
+       /* return "Win!"; */
+        alert ("Win!");
+    }
 
+    else if (playerSelection === 'rock' &&  computerSelection === 'scissors') {
+      /*  return "Win!"  */
+        alert ("Win!");
+    }
 
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        /*  return "Win!"  */
+        alert ("Win!");
+    }
+
+    else {
+        alert ('Loser! ${computerSelection} beats ${playerSelection}');
+        /* return '"Loser! ${computerSelection} beats ${playerSelection}"' */
+    }
+
+    
 }
+
+playRound();
+
