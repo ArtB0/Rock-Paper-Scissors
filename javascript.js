e0f9af6d29dc1c51f98a241fae905f789e463abb
Art <artbogdanovics@gmail.com>
2022-07-09 21:48:21 +0300

    added counter

--- javascript.js
+++ javascript.js
@@ -18,18 +18,22 @@ function playRound (playerSelection, computerSelection) {
      }
 
      else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore ++;
         return "Win!"; 
     }
 
     else if (playerSelection === 'rock' &&  computerSelection === 'scissors') {
+        playerScore ++;
         return "Win!"; 
     }
 
     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore ++;
         return "Win!"; 
     }
 
     else {
+        computerScore ++;
         return 'Loser!'
     }
     
@@ -41,11 +45,15 @@ function game () {
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt ('Rock, Paper or Scissors?');
         console.log(playRound(playerSelection, computerSelection));
+        console.log('player:', playerScore , 'computer:' , computerScore);
     }
 }
 
 
 const computerSelection = computerPlay();
+
+let playerScore = 0
+let computerScore = 0
 
 
 game ();
